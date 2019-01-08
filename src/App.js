32278b92ca0626d479dc0b0bf81966eef6010bd2
Itamar Tati <itamar.tati1994@gmail.com>
2019-01-08 08:33:35 +0000

    Added Anime T.V show covers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-//import gowild from './Images/gowild.jpeg/'
-
+import cowboyBebop from './images/cowboybebop.jpg';
+import dragonBall from './images/dragonball.jpg';
+import eva from './images/eva.jpg';
+import ruruoniKenshin from './images/ruruonikenshin.jpg';
+import schoolRumble from './images/schoolrumble.jpg';
+import yuYuHakusho from './images/yuyuhakusho.jpg';
 
 import './App.css';
 
@@ -8,23 +12,41 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <h1>Kodflix (Anime Divison)</h1>
       <br /><br /><br />
-      <div className = 'container' >
-        <div className = 'item'>Go Wild</div>
-        <div className = 'item'>Infected</div>
-        <div className = 'item'>The Blenders</div>
-      </div> 
-      <div className = 'container' >
-        <div className = 'item'>The Rain</div>
-        <div className = 'item'>Priest</div>
-        <div className = 'item'>Walking Dead</div>
-      </div>
-
-
-    </div>
-
-
       
+      <div className = 'container'>
+        
+        <div className = 'item'>
+          <img src={cowboyBebop} alt='Cowboy Bebop'/>
+        </div>
+        
+        <div className = 'item'>
+          <img src={dragonBall} alt='Dragon Ball Z'/>
+        </div>
+        
+        <div className = 'item'>
+          <img src={eva} alt='Neon Genesis Evangelian'/>
+        </div>
+        
+          </div>             
+      
+      <div className = 'container'>
+        
+        <div className = 'item'>
+          <img src={ruruoniKenshin} alt='Ruruoni Kenshin'/>
+        </div>
+        
+        <div className = 'item'>
+          <img src={schoolRumble} alt='School Rumble'/>
+        </div>
+        
+        <div className = 'item'>
+          <img src={yuYuHakusho} alt='Yu Yu Hakusho'/>
+        </div>
+        
+        </div>
+        </div> 
     );
   }
 }
