ca09f230df2e58a2135902a96de9316fc7ccf71d
Itamar Tati <itamar.tati1994@gmail.com>
2019-01-08 13:18:29 +0000

    Added the TV show title overlay on hover-over

--- src/App.js
+++ src/App.js
@@ -12,47 +12,61 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>Kodflix (Anime Divison)</h1>
-      <br /><br /><br />
+        <h1>Kodflix (Anime Divison)</h1>
+        <br /><br /><br />
       
-      <div className = 'container'>
+        <div className = 'container'>
+          
+          <div className = 'item'>
+            <img src={cowboyBebop} alt='Cowboy Bebop'/>
+              <div className = 'overlay'>
+                <h2>Cowboy Bebop</h2>
+              </div>
+          </div>
         
-        <div className = 'item'>
-          <img src={cowboyBebop} alt='Cowboy Bebop'/>
-            <h2>Cowboy Bebop</h2>
-        </div>
+          <div className = 'item'>
+            <img src={dragonBall} alt='Dragon Ball'/>
+              <div className = 'overlay'>
+                <h2>Dragon Ball</h2>
+              </div>
+          </div>
         
-        <div className = 'item'>
-          <img src={dragonBall} alt='Dragon Ball'/>
-            <h2>Dragon Ball</h2>
-        </div>
+          <div className = 'item'>
+            <img src={eva} alt='Neon Genesis Evangelian'/>
+              <div className = 'overlay'> 
+                <h2>Neon Genesis Evangelian</h2>
+              </div>
+          </div>
         
-        <div className = 'item'>
-          <img src={eva} alt='Neon Genesis Evangelian'/>
-            <h2>Neon Genesis Evangelian</h2>
-        </div>
-        
-          </div>             
+        </div> 
+                     
       
       <div className = 'container'>
         
         <div className = 'item'>
           <img src={ruruoniKenshin} alt='Ruruoni Kenshin'/>
-            <h2>Ruruoni Kenshin</h2>
+            <div className = 'overlay'>
+              <h2>Ruruoni Kenshin</h2>
+            </div>
         </div>
         
         <div className = 'item'>
           <img src={schoolRumble} alt='School Rumble'/>
-            <h2>School Rumble</h2>
+            <div className = 'overlay'>
+              <h2>School Rumble</h2>
+            </div>
         </div>
         
         <div className = 'item'>
           <img src={yuYuHakusho} alt='Yu Yu Hakusho'/>
-            <h2>Yu Yu Hakusho</h2>
+            <div className = 'overlay'>
+              <h2>Yu Yu Hakusho</h2>
+            </div> 
         </div>
         
-        </div>
-        </div> 
+      </div>
+    </div>
+         
     );
   }
 }
