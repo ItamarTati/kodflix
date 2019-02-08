0912aad19d05553bb97ee0d3328bcecf9017a1e9
Itamar Tati <itamar.tati1994@gmail.com>
2019-02-08 13:21:23 +0000

    Fixed bug on task 23

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,12 +5,11 @@ const getShows = require('./shows');
 const path = require('path');
 
 app.get('/rest/shows', (req, res) => res.send(getShows()))
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-app.use(express.static(path.join(__dirname, '.../.../build')));
+app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '.../.../build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
