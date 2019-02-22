44323990b007ac358f989cec6231f042ec9474a1
Itamar Tati <itamar.tati1994@gmail.com>
2019-02-22 11:01:21 +0000

    changes to app.js

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3001
-const getShows = require('./shows');
+const express = require('express');
 const path = require('path');
+const app = express();
+const port = process.env.PORT || 3001;
+const getShows = require('./shows');
 
-app.get('/rest/shows', (req, res) => res.send(getShows()))
+app.get('/rest/shows', (req, res) => res.send(getShows()));
+
 
 app.use(express.static(path.join(__dirname, '../../build')));
 
@@ -12,4 +13,4 @@ app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}`));
