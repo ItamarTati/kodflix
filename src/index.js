6fcdec85eb271af1b5f14d7a23cd38adb6166795
Anshuman Pandey <GV Tech>
2019-02-23 17:05:11 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './frontend/App';
+import App from './src/frontend/App';
 import registerServiceWorker from './frontend/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
