9092a7b3d700b311a97052ede68f887e9c5bcc20
Anshuman Pandey <GV Tech>
2019-02-23 16:44:13 +0530

    make it better

--- src/backend/app.js
+++ src/backend/app.js
@@ -7,10 +7,11 @@ const getShows = require('./shows');
 app.get('/rest/shows', (req, res) => res.send(getShows()));
 
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../../build')));
 
+console.log(path.join(__dirname, '../../build', 'index.html'));
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
